6555168da19fb8ffb9df2f0f717c63a60fd4192c
c00cjz00 <summerhill001@gmail.com>
2024-05-04 19:05:01 +0800

    Update 02-install_package.sh

--- class_20240408/02-install_package.sh
+++ class_20240408/02-install_package.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-mkdir -p local/app cache/
+mkdir -p local/app cache/ conda/
 
 singularity exec --nv --pwd $HOME/.local/app -B ./local:$HOME/.local -B ./cache:$HOME/.cache \
 images/c00cjz00_cuda11.8_pytorch_2.1.2-cuda11.8-cudnn8-devel.sif \
-bash -c "git clone https://github.com/hiyouga/LLaMA-Factory.git; \
-pip install -r LLaMA-Factory/requirements.txt"
-
+bash -c "git clone --depth 1 --branch v0.7.0 https://github.com/hiyouga/LLaMA-Factory.git; \
+cd LLaMA-Factory; \
+pip install .[bitsandbytes]"
